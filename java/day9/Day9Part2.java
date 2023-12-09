5f972001c99866270fbb6c138d49a0d1876c769c
Nick Edelenbos <81759878+nickedelenbos@users.noreply.github.com>
2023-12-09 13:09:10 +0100

    Solve day9pt2

---
+++ java/day9/Day9Part2.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class Day9Part2 {
+    public static void main(String[] args) throws IOException {
+        List<List<Integer>> values = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("day9/input.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                values.add(Arrays.stream(line.split(" ")).map((Integer::parseInt)).toList());
+            }
+
+            System.out.println(values.stream().map(Day9Part2::solve).mapToInt(p -> p.addValue).sum());
+        }
+    }
+
+    private static Pair solve(List<Integer> list) {
+        if (list.stream().allMatch(n -> n.equals(list.getFirst()))) {
+            return new Pair(1, list.getFirst());
+        } else {
+            Pair p = solve(IntStream.range(0, list.size() - 1).mapToObj(i -> list.get(i + 1) - list.get(i)).toList());
+            return new Pair(p.count + 1, list.getFirst() - p.addValue);
+        }
+    }
+
+    public record Pair(int count, int addValue) { }
+}
